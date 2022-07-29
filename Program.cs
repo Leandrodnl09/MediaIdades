fbb31a72a88862e558093289e88b8563a1d52c1a
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-07-29 18:00:18 -0300

    MediaIdades

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Exercício do site URI "Idades"
+// Este programa o usuário ira digitar uma quantidade indeterminada de idades até que seja digitado numero negativo. No final mostra a media de toas as idades.
+
+double Media;
+int  Soma, Cont;
+
+int Idade = int.Parse(Console.ReadLine());
+
+Cont = 0;
+Soma = 0;
+while (Idade >= 0)
+{
+    Soma = Idade + Soma;
+    Cont = Cont + 1;
+    
+    Idade = int.Parse(Console.ReadLine());
+}
+
+Media = (double) Soma / Cont;
+Console.WriteLine(Media.ToString("F2"));
